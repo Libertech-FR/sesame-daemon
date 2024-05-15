9d5b138df16b8d89bab6b16522c4b7baabf12ad6
Tacx <12997062+tacxou@users.noreply.github.com>
2024-05-15 15:38:03 +0200

    chore: Update release.yml and remove unused imports in AppService

--- src/_common/tasks/executor.task.ts
+++ src/_common/tasks/executor.task.ts
@@ -13,39 +13,44 @@ export async function executorTask(
     const jobDataArg = JSON.stringify(job.data);
     // const escapedJobDataArg = `'${jobDataArg.replace(/'/g, "'\\''")}'`;
 
-    const child = spawn(join(command), {
-      shell: options?.shell ?? true,
-      // stdio: 'pipe',
-    });
-
-    child.stdin.write(jobDataArg);
-    child.stdin.end();
-
-    let outputChunk = '';
-    let errorChunk = '';
-
-    child.stdout.on('data', (data) => {
-      console.log(`stdout: ${data}`);
-      outputChunk += data;
-    });
-
-    child.stderr.on('data', (data) => {
-      console.error(`stderr: ${data}`);
-      errorChunk += data;
-    });
-
-    child.on('close', (code) => {
-      console.log(`Le processus enfant s'est terminé avec le code ${code}`);
-      resolve({
-        status: code,
-        output: outputChunk?.toString(),
-        error: errorChunk?.toString(),
+    try {
+      const child = spawn(join(command), {
+        shell: options?.shell ?? true,
+        stdio: 'pipe',
       });
-    });
 
-    child.on('error', (spawnError) => {
-      console.error('Erreur lors du lancement du processus enfant', spawnError);
-      reject(spawnError);
-    });
+      child.stdin.write(jobDataArg);
+      child.stdin.end();
+
+      let outputChunk = '';
+      let errorChunk = '';
+
+      child.stdout.on('data', (data) => {
+        console.log(`stdout: ${data}`);
+        outputChunk += data;
+      });
+
+      child.stderr.on('data', (data) => {
+        console.error(`stderr: ${data}`);
+        errorChunk += data;
+      });
+
+      child.on('close', (code) => {
+        console.log(`Le processus enfant s'est terminé avec le code ${code}`);
+        resolve({
+          status: code,
+          output: outputChunk?.toString(),
+          error: errorChunk?.toString(),
+        });
+      });
+
+      child.on('error', (spawnError) => {
+        console.error('Erreur lors du lancement du processus enfant', spawnError);
+        reject(spawnError);
+      });
+    } catch (error) {
+      console.error('Erreur lors de l\'exécution de la tâche', error);
+      reject(error);
+    }
   });
 }
