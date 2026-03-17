b9d5a70e7a798fd10f32863e968924ed9f28074d
Tacx <12997062+tacxou@users.noreply.github.com>
2026-03-17 15:49:58 +0100

    Enhance logging in DumpPackageConfigExecutor to include selected package.json fields

--- src/backend-runner/_executors/dump-package-config.executor.ts
+++ src/backend-runner/_executors/dump-package-config.executor.ts
@@ -1,14 +1,18 @@
+import { pick } from 'radash';
 import { BackendRunnerService } from '../backend-runner.service';
 import { ExecutorExecuteResponseInterface, ExecutorInterface } from '../executors.interface';
+import { PackageJson } from 'types-package-json';
 
 export class DumpPackageConfigExecutor implements ExecutorInterface {
   public constructor(public service: BackendRunnerService) { }
 
   public async execute({ job }): Promise<ExecutorExecuteResponseInterface> {
-    return {
-      status: 0,
-      jobId: job.id,
-      data: [this.service.packageJson],
-    };
+    this.service.logger.debug('Dump package config: ' + JSON.stringify(pick(this.service.packageJson, [
+      'name',
+      'description',
+      'version',
+    ] as any) as Pick<PackageJson, 'name' | 'description' | 'version'>));
+
+    return { status: 0, jobId: job.id, data: [this.service.packageJson] };
   }
 }
